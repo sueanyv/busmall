e218806550953e492ae09853c60d29c9bbf7e83e
sugey <valenciasue@hotmail.com>
2017-06-21 07:38:19 -0700

    added more to app.js

--- app.js
+++ app.js
@@ -1,38 +1,109 @@
 'use strict';
 
+var imgContainer = document.getElementById('image-container');
+var productImg = [];
+var newImg = [];
+var previousImages = [];
+var left = document.getElementById('left');
+var middle = document.getElementById('middle');
+var right = document.getElementById('right');
+var totalClicks = 0;
+var canvas = document.getElementById('canvas');
 
-var productimg = [];
+var context = canvas.getContext('2d');
 
 
+var leftImg;
+var centerImg;
+var rightImg;
+
+new Product('bag', 'img/bag.jpg', 0, 0);
+new Product('banana', 'img/banana.jpg', 0, 0);
+new Product('bathroom', 'img/bathroom.jpg', 0, 0);
+new Product('boots', 'img/boots.jpg', 0, 0);
+new Product('breakfast', 'img/breakfast.jpg', 0, 0);
+new Product('bubblegum', 'img/bubblegum.jpg', 0, 0);
+new Product('chair', 'img/chair.jpg', 0, 0);
+new Product('cthulhu', 'img/cthulhu.jpg', 0, 0);
+new Product('dog-duck', 'img/dog-duck.jpg', 0, 0);
+new Product('pen', 'img/pen.jpg', 0, 0);
+new Product('pet-sweep', 'img/pet-sweep.jpg', 0, 0);
+new Product('scissors', 'img/scissors.jpg', 0, 0);
+new Product('shark', 'img/shark.jpg', 0, 0);
+new Product('sweep', 'img/sweep.png', 0, 0);
+new Product('tautaun', 'img/tauntaun.jpg', 0, 0);
+new Product('unicorn', 'img/unicorn.jpg', 0, 0);
+new Product('usb', 'img/usb.gif', 0, 0);
+new Product('water-can', 'img/water-can.jpg', 0, 0);
+new Product('wine-glass', 'img/wine-glass.jpg', 0, 0);
 
 // product constructor
-
-function Product(name, path, views, clicks) {
-  this.name = name;
+function Product(imgName, path, views, clicks) {
+  this.imgName = imgName;
   this.path = path;
   this.views = views;
   this.click = clicks;
-  productimg.push(this);
+  productImg.push(this);
+};
+
+function turnArrayIntoImages(product, img) {
+  var att = document.createAttribute('src');
+  att.value = product.path;
+  img.setAttributeNode(att);
+  // left.setAttribute();
 }
 
-new Product('bag', 'img/bag.jpg');
-new Product('banana', 'img/banana.jpg');
-new Product('bathroom', 'img/bathroom.jpg');
-new Product('boots', 'img/boots.jpg');
-new Product('breakfast', 'img/breakfast.jpg');
-new Product('bubblegum', 'img/bubblegum.jpg');
-new Product('chair', 'img/chair.jpg');
-new Product('cthulhu', 'img/cthulhu.jpg');
-new Product('dogduck', 'img/dog-duck.jpg');
-new Product('dragon', 'img/dragon.jpg');
-new Product('pen', 'img/pen.jpg');
-new Product('petsweep', 'img/pet-sweep.jpg');
-new Product('scissors', 'img/scissors.jpg');
-new Product('shark', 'img/shark.jpg');
-new Product('sweep', 'img/sweep.jpg');
-new Product('tautaun', 'img/tauntaun.jpg');
-new Product('unicorn', 'img/unicorn.jpg');
-new Product('usb', 'img/usb.jpg');
-new Product('watercan', 'img/water-can.jpg');
-new Product('wine-glass', 'img/wine-glass.jpg');
-console.log(productimg);
+function render() {
+  var imgRandom = function() {
+    leftImg = Math.floor(Math.random() * productImg.length);
+    centerImg = Math.floor(Math.random() * productImg.length);
+    rightImg = Math.floor(Math.random() * productImg.length);
+    newImg = [];
+    newImg.push(leftImg, centerImg, rightImg);
+  };
+  imgRandom();
+  //checking for duplicate images
+  while ((leftImg === centerImg) || (centerImg === rightImg) || (leftImg === rightImg) || newImg[0] === previousImages[0] || newImg[0] === previousImages[1] || newImg[0] === previousImages[2] || newImg[1] === previousImages[0] || newImg[1] === previousImages[1] || newImg[1] === previousImages[2] || newImg[2] === previousImages[0] || newImg[2] === previousImages[1] || newImg[2] === previousImages[2]) {
+    imgRandom();
+  }
+  turnArrayIntoImages(productImg[leftImg], left);
+  turnArrayIntoImages(productImg[centerImg], middle);
+  turnArrayIntoImages(productImg[rightImg], right);
+}
+imgContainer.addEventListener('click', handleImageClick);
+
+function handleImageClick(event) {
+  var clickedProduct;
+  switch (event.target.id) {
+  case 'left':
+    clickedProduct = productImg[newImg[0]];
+    break;
+  case 'middle':
+    clickedProduct = productImg[newImg[1]];
+    break;
+  case 'right':
+    clickedProduct = productImg[newImg[2]];
+    break;
+  }
+  clickedProduct.click += 1;
+  totalClicks += 1;
+  if (totalClicks >= 25) {
+    showResuts();
+    imgContainer.removeEventListener('click', handleImageClick);
+    displayChart();
+  } else {
+    render();
+
+  }
+}
+
+function showResuts() {
+
+  for (var i = 0; i < productImg.length; i++) {
+    console.log(productImg[i].imgName + ';' + productImg[i].click + '/' + productImg[i].views);
+  }
+
+}
+
+
+render();
